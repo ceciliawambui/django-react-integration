8b599b26a8a862fe534f58fcd2326877e81ac8bc
ceciliawambui <ceciliawambui026@gmail.com>
2026-06-04 13:21:44 +0300

    Integrating React with Django backend

---
+++ students_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.db import models
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
+    grade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
